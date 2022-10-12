b433b72e258ac3822a7a09f3b6fcc071aac436b2
Gloria Paulina Gonzalez Hernandez <ghpaulina3@gmail.com>
2022-10-12 15:53:41 -0600

    commit tree 149f2cbc34681667331fc2f7376df2196912433e

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+    if (someValue > 2500) {
+      return 'No can do.';
+    }
+    else {
+      if (someValue <= 400) {
+        return 'This one is on me!';
+      }
+    else {
+      if (someValue <= 2000) {
+      return 'That will be twenty bucks.';
+    }
+    else {
+      if (someValue > 2000) {
+      return 'I will gladly take your thirty bucks.';
+    }}
+  }}
+  }
+
+
+function ternaryCheckCity(city) {
+  let result
+  result = city = 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+const switchOnCharmFromTip = "generous";
+let result; 
+
+switch(switchOnCharmFromTip) {
+  case "generous":
+    result = "Thank you so much.";
+    break;
+  case "not so generous":
+    result = "Thank you.";
+    break;
+  case "anything else": 
+    result = "Bye.";
+    break;
+    default: 
+    result = "Goodbye.";
 }
+console.log(result);
